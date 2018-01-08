5496d551967f30acd24a41637b056b711942392f
zoumana-keita <zoumana930@hotmail.fr>
2018-01-08 15:35:31 +0100

    Installation travis dans le projet

--- src/main/java/fr/esiea/service/CityService.java
+++
@@ -1,6 +0,0 @@
-package fr.esiea.service;
-
-import fr.esiea.model.domain.City;
-
-public interface CityService extends BaseService<City, Integer> {
-}
--- src/main/java/fr/esiea/service/impl/CityServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package fr.esiea.service.impl;
-
-import fr.esiea.model.domain.City;
-import fr.esiea.service.CityService;
-import org.springframework.stereotype.Service;
-
-@Service("cityService")
-public class CityServiceImpl extends BaseServiceImpl<City, Integer> implements CityService {
-}
